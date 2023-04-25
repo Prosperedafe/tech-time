04b454d1eb3a9c11205683e60824d3aa3d67c53c
prosper edafe <mamusedafe19@gmail.com>
2023-04-25 23:45:34 +0100

    wrap

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import Header from "./components/header"
+import Landing from "./landing"
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <Header />
+      <Landing />
+    </>
+  )
+}
+
+export default App;
